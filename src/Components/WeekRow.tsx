cf9111dd46e9cc4ffdb2f05d37c319eb6bad53dc
Sol Elan <solipod8@gmail.com>
2024-10-08 13:29:52 +0900

    Style spinner

--- src/Components/WeekRow.tsx
+++ src/Components/WeekRow.tsx
@@ -26,10 +26,10 @@ const Spinner = styled.div`
   border: 2px solid #f3f3f3;
   border-top: 2px solid #5d718c; /* Dark blue color */
   border-radius: 50%;
-  width: 12px;
-  height: 12px;
+  width: 10px;
+  height: 10px;
+  margin-bottom: 4px;
   animation: spin 1s linear infinite;
-  
   @keyframes spin {
     0% { transform: rotate(0deg); }
     100% { transform: rotate(360deg); }
